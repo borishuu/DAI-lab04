63caf31dafd2d9a32c0deeb034e85edf35ab5041
borishuu <boris.hutzli@heig-vd.ch>
2023-11-09 11:01:38 +0100

    created maven project

---
+++ smtpclient/src/main/java/ch/heig/dai/lab/smtp/App.java
@@ -0,0 +1,13 @@
+package ch.heig.dai.lab.smtp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
